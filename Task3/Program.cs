673a6b4575d062c3159a85f01495dd26f6017078
Tatyana Stasenko <sale_rim@mail.ru>
2024-03-15 02:09:34 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1 
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == array.Length - 1)
+        {
+            Console.Write($"{array[i]}");
+        }
+        else
+        {
+            Console.Write($"{array[i]}, ");
+        }
+    }
+    Console.Write("]");
+}
+
+
+void PrintReverseArray(int[] array, int count)
+{
+    if (count == 0) return;
+
+    Console.Write($"{array[count - 1]} ");
+    PrintReverseArray(array, count - 1);
+}
+
+int minmum = 1;
+int maxmum = 100;
+
+Console.WriteLine("Введите размер массива");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] arrayNew = CreateArrayRndInt(count, minmum, maxmum);
+PrintArray(arrayNew);
+Console.Write(" => ");
+PrintReverseArray(arrayNew, arrayNew.Length);
+Console.WriteLine();
